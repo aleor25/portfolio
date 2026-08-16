73bba92ff8974efadbf943253bd8dec13305c128
aleor25 <aleor25@users.noreply.github.com>
2026-08-15 23:02:54 -0600

    refactor: remove legacy components and unused sections

--- components/templates/PortfolioTemplate.tsx
+++ components/templates/PortfolioTemplate.tsx
@@ -1,17 +1,18 @@
 import Header from '@/components/organisms/Header'
 import Footer from '@/components/organisms/Footer'
-import AboutMeSection from '@/components/organisms/AboutMeSection'
-import ProjectsSection from '@/components/organisms/ProjectsSection'
-import Navbar from '../organisms/NavBar'
+import AboutMe from '@/components/organisms/AboutMe'
+import Projects from '@/components/organisms/Projects'
 import ScrollToTop from '../atoms/ScrollToTop'
+import Experience from '../organisms/Experience'
 
 export default function PortfolioTemplate() {
     return (
-        <div className="min-h-screen bg-[#121212] text-white selection:bg-sky-500/30">
+        <div className="min-h-screen bg-slate-200 dark:bg-[#121212] text-neutral-900 dark:text-white selection:bg-sky-500/30 transition-colors duration-300">
             <Header />
             <main className="flex flex-col pb-20">
-                <AboutMeSection />
-                <ProjectsSection />
+                <AboutMe />
+                <Experience />
+                <Projects />
             </main>
             <Footer />
             <ScrollToTop />
